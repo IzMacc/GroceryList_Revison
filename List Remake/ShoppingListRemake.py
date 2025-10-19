019cca54aaf7f01c5a285b37a984f8dbc702707c
IzMacc <157656108+IzMacc@users.noreply.github.com>
2025-10-19 19:38:40 -0400

    Edit4
    Code for .txt files were not working properly. Redid code so now only 1 .txt file is required instead of 2

--- List Remake/ShoppingListRemake.py
+++ List Remake/ShoppingListRemake.py
@@ -5,48 +5,57 @@ Created on Sun Oct 19 18:21:44 2025
 @author: Isabella
 """
 
-def convert_menu_to_dictionary(menu_file):
-    menu_dict = {}
-    with open(menu_file, 'r') as file:
-        for line in file:
-            # Split the line into recipe name and ingredients
-            recipe, ingredients = line.strip().split(maxsplit=1)
-            # Add the recipe and ingredients to the dictionary
-            menu_dict[recipe] = ingredients.split()
-    return menu_dict
 
-def display_recipes(menu_dict):
-    # Display all available recipes
+# Embedded menu as a dictionary
+menu_dict = {
+    "Pasta": ["noodles", "tomato sauce", "cheese", "Basil", "meatballs"],
+    "Salad": ["lettuce", "tomato", "cucumber", "dressing"],
+    "Tacos": ["tortillas", "beef", "cheese", "salsa", "lettuce"],
+    "Sandwich": ["bread", "ham", "cheese", "lettuce", "tomato"]
+}
+
+# Show the user all available recipes
+def show_recipes(menu):
     print("\nAvailable Recipes:")
-    for i, recipe in enumerate(menu_dict.keys(), 1):
+    for i, recipe in enumerate(menu.keys(), start=1):
         print(f"{i}. {recipe}")
-    print()
 
+# Let the user pick one or more recipes they want to make
+def choose_recipes(menu):
+    show_recipes(menu)
+    choices = input("\nEnter the numbers of the corresponding recipes you want to make: ")
+    chosen = []
+    for choice in choices.split(','):
+        choice = choice.strip()
+        if choice.isdigit():
+            index = int(choice) - 1
+            if 0 <= index < len(menu):
+                recipe = list(menu.keys())[index]
+                chosen.append(recipe)
+    return chosen
 
-def choose_recipes(menu_dict):
-    # Let the user pick which recipes they want to make
-    display_recipes(menu_dict)
-    choices = input("Enter the numbers of the recipes you want: ")
-    selected_recipes = []
-    for num in choices.split(','):
-        num = num.strip()
-        if num.isdigit() and 1 <= int(num) <= len(menu_dict):
-            selected_recipes.append(list(menu_dict.keys())[int(num) - 1])
-    return selected_recipes
-
-def check_ingredients(menu_dict, selected_recipes):
-    # For each selected recipe, ask if the user has the ingredients
-    grocery_items = set()
-    for recipe in selected_recipes:
-        print(f"\nChecking ingredients for '{recipe}':")
-        for ingredient in menu_dict[recipe]:
-            have_it = input(f"Do you have '{ingredient}'? (cay/n): ").lower()
+# Ask if user has each ingredient; collect missing ones
+def check_ingredients(menu, chosen_recipes):
+    grocery_list = []
+    for recipe in chosen_recipes:
+        print(f"\nChecking ingredients for {recipe}:")
+        for ingredient in menu[recipe]:
+            while True:
+                have_it = input(f"Do you have {ingredient}? (y/n): ").strip().lower()
+                if have_it in ['y', 'n']:
+                    break
+                else:
+                    print("Please enter 'y' or 'n'.")
             if have_it != 'y':
-                grocery_items.add(ingredient)
-    return sorted(grocery_items)
+                grocery_list.append(ingredient)
+    return grocery_list
 
-def display_grocery_list(grocery_list):
-    # Display the final grocery list without duplicates
+# Remove duplicates from grocery list
+def remove_duplicates(grocery_list):
+    return list(set(grocery_list))
+
+# Show the grocery list to the user
+def show_grocery_list(grocery_list):
     print("\nGrocery List:")
     if grocery_list:
         for item in grocery_list:
@@ -54,31 +63,28 @@ def display_grocery_list(grocery_list):
     else:
         print("You already have everything you need!")
 
+# Save the grocery list to a file
+def save_grocery_list(filename, grocery_list):
+    with open(filename, 'w') as file:
+        if grocery_list:
+            for item in grocery_list:
+                file.write(f"{item}\n")
+        else:
+            file.write("You already have everything you need!\n")
+    print(f"\nGrocery list saved to '{filename}'.")
 
-def save_grocery_list_to_file(grocery_list, file_name):
-    # Save the grocery list to an external file
-    with open(file_name, 'w') as file:
-        for item in grocery_list:
-            file.write(item + '\n')
-    print(f"\nGrocery list saved to '{file_name}'.")
-
+# Main program flow
 def main():
-    # Convert menu.txt to dictionary
-    menu_file = "menu.txt"
-    menu_dict = convert_menu_to_dictionary(menu_file)
+    chosen_recipes = choose_recipes(menu_dict)
+    if not chosen_recipes:
+        print("No recipes selected.")
+        return
 
-    # Let the user pick recipes
-    selected_recipes = choose_recipes(menu_dict)
+    grocery_list = check_ingredients(menu_dict, chosen_recipes)
+    grocery_list = remove_duplicates(grocery_list)
 
-    # Check ingredients and create grocery list
-    grocery_list = check_ingredients(menu_dict, selected_recipes)
-
-    # Display grocery list
-    display_grocery_list(grocery_list)
-
-    # Save grocery list to external file
-    save_grocery_list_to_file(grocery_list, "grocerylist.txt")
-
+    show_grocery_list(grocery_list)
+    save_grocery_list("grocerylist.txt", grocery_list)
 
 if __name__ == "__main__":
     main()
